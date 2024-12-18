a6caaca5d1469702f98b9460ae395c7d8087993a
iitkgpnltm <iitkgpnltm@gmail.com>
2024-12-18 12:42:09 +0200

    18/12-Jagabandhu

---
+++ load_known_unknown_speaker_label_evaluation.py
@@ -0,0 +1,6 @@
+from scipy.io import loadmat
+
+# Load the .mat file
+file_path = "/home/mishra/Research_Spoofing/aasist-main/Closed_Set_Experiments/Dataset/unknown_1_known_0_label_closeset_asv2019.mat"
+data = loadmat(file_path)
+#%%
